c244712ea817da7f96f7e07166c347b4be478376
Amal1999 <sammari_a@outlook.fr>
2024-02-27 16:01:19 +0100

    try with one task

--- dags/my_own_dag.py
+++ dags/my_own_dag.py
@@ -1,40 +1,40 @@
 import os
 import requests
-from datetime import timedelta
-
-from airflow import DAG
-from airflow.operators.dummy_operator import DummyOperator
-from airflow.utils.dates import days_ago
+from pendulum import datetime
 from airflow.decorators import task, dag
 from airflow.providers.postgres.hooks.postgres import PostgresHook
 
-with DAG(
-    "my_own_dag",
-    schedule_interval=None,
-    start_date=days_ago(0),
-    dagrun_timeout=timedelta(minutes=60),
-) as dag:
-    dummy_task = DummyOperator(task_id="dummy_task")
+@dag(
+    start_date=datetime(2022,11,1),
+    schedule="@daily",
+    catchup=False
+)
+def my_own_dag():
 
-@task
-def get_data():
-    # NOTE: configure this as appropriate for your airflow environment
-    data_path = "/opt/airflow/dags/files/employees.csv"
-    os.makedirs(os.path.dirname(data_path), exist_ok=True)
+        @task
+        def get_data():
+            # NOTE: configure this as appropriate for your airflow environment
+            data_path = "/opt/airflow/dags/files/employees.csv"
+            os.makedirs(os.path.dirname(data_path), exist_ok=True)
 
-    url = "https://raw.githubusercontent.com/apache/airflow/main/docs/apache-airflow/tutorial/pipeline_example.csv"
+            url = "https://raw.githubusercontent.com/apache/airflow/main/docs/apache-airflow/tutorial/pipeline_example.csv"
 
-    response = requests.request("GET", url)
+            response = requests.request("GET", url)
 
-    with open(data_path, "w") as file:
-        file.write(response.text)
+            with open(data_path, "w") as file:
+                file.write(response.text)
 
-    postgres_hook = PostgresHook(postgres_conn_id="tutorial_pg_conn")
-    conn = postgres_hook.get_conn()
-    cur = conn.cursor()
-    with open(data_path, "r") as file:
-        cur.copy_expert(
-            "COPY employees_temp FROM STDIN WITH CSV HEADER DELIMITER AS ',' QUOTE '\"'",
-            file,
-        )
-    conn.commit()
+            postgres_hook = PostgresHook(postgres_conn_id="tutorial_pg_conn")
+            conn = postgres_hook.get_conn()
+            cur = conn.cursor()
+            with open(data_path, "r") as file:
+                cur.copy_expert(
+                    "COPY employees_temp FROM STDIN WITH CSV HEADER DELIMITER AS ',' QUOTE '\"'",
+                    file,
+                )
+            conn.commit()
+
+
+        get_data()
+
+my_own_dag()
