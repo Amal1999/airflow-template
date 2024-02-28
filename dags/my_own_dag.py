07e872e35984a58ec9b819c41135b655004d0bda
Amal1999 <sammari_a@outlook.fr>
2024-02-28 16:26:16 +0100

    h

--- dags/my_own_dag.py
+++ dags/my_own_dag.py
@@ -27,15 +27,15 @@ def my_own_dag():
             with open(data_path, "w") as file:
                 file.write(response.text)
 
-            postgres_hook = PostgresHook(postgres_conn_id="tutorial_pg_conn")
-            conn = postgres_hook.get_conn()
-            cur = conn.cursor()
-            with open(data_path, "r") as file:
-                cur.copy_expert(
-                    "COPY employees_temp FROM STDIN WITH CSV HEADER DELIMITER AS ',' QUOTE '\"'",
-                    file,
-                )
-            conn.commit() 
+            # postgres_hook = PostgresHook(postgres_conn_id="tutorial_pg_conn")
+            # conn = postgres_hook.get_conn()
+            # cur = conn.cursor()
+            # with open(data_path, "r") as file:
+            #     cur.copy_expert(
+            #         "COPY employees_temp FROM STDIN WITH CSV HEADER DELIMITER AS ',' QUOTE '\"'",
+            #         file,
+            #     )
+            # conn.commit() 
 
         get_data()
        
