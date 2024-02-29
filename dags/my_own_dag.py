257566eef8caaea93a5a51a42a0ceed5e2bfd556
Amal1999 <sammari_a@outlook.fr>
2024-02-29 13:04:29 +0100

    g

--- dags/my_own_dag.py
+++ dags/my_own_dag.py
@@ -18,7 +18,7 @@ def my_own_dag():
         @task
         def get_data():
             # NOTE: configure this as appropriate for your airflow environment
-            data_path = "/opt/airflow/dags/files/employees.csv"
+            data_path = "/opt/airflow/dags/try/employees.csv"
             os.makedirs(os.path.dirname(data_path), exist_ok=True)
             # url = "https://raw.githubusercontent.com/apache/airflow/main/docs/apache-airflow/tutorial/pipeline_example.csv"
 
